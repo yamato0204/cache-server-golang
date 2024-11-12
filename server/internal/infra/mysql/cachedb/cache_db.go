f85b662a919bd93757f88ab3426bf2a2a0a73b03
yamato0204 <tech.yama.0204@gmail.com>
2024-11-12 19:36:02 +0900

    SyncedToDB実装

--- server/internal/infra/mysql/cachedb/cache_db.go
+++ server/internal/infra/mysql/cachedb/cache_db.go
@@ -7,22 +7,22 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-// type BulkExecutor interface {
-// 	BulkInsert(ctx context.Context, tx *sqlx.Tx, contents []CacheContent) error
-// 	BulkUpdate(ctx context.Context, tx *sqlx.Tx, contents []CacheContent) error
-// 	BulkDelete(ctx context.Context, tx *sqlx.Tx, contents []CacheContent) error
-// }
-
-type CacheDB struct {
-	db *sqlx.DB
-	tx *sqlx.Tx
-	//modelBulkExecutorMap map[string]BulkExecutor
+type BulkExecutor interface {
+	BulkInsert(ctx context.Context, tx *sqlx.Tx, contents []CacheContent) error
+	BulkUpdate(ctx context.Context, tx *sqlx.Tx, contents []CacheContent) error
+	BulkDelete(ctx context.Context, tx *sqlx.Tx, contents []CacheContent) error
 }
 
-func NewCacheDB(db *sqlx.DB) *CacheDB {
+type CacheDB struct {
+	db                   *sqlx.DB
+	tx                   *sqlx.Tx
+	modelBulkExecutorMap map[string]BulkExecutor
+}
+
+func NewCacheDB(db *sqlx.DB, modelBulkExecutorMap map[string]BulkExecutor) *CacheDB {
 	return &CacheDB{
-		db: db,
-		//modelBulkExecutorMap: modelBulkExecutorMap,
+		db:                   db,
+		modelBulkExecutorMap: modelBulkExecutorMap,
 	}
 }
 
--- server/internal/infra/mysql/cachedb/cache_db_sync.go
+++ server/internal/infra/mysql/cachedb/cache_db_sync.go
@@ -38,3 +38,58 @@ func (cdb *CacheDB) Rollback(ctx context.Context) error {
 
 	return cdb.tx.Rollback()
 }
+
+func (cdb *CacheDB) SyncedToDB(ctx context.Context) (map[string]map[CacheStatus][]CacheContent, error) {
+	if cdb.tx == nil {
+		return nil, errors.New("transaction is not started")
+	}
+
+	cacheManager, err := extractDBOperationCacheManager(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	cachesMapByStatus := make(map[string]map[CacheStatus][]CacheContent)
+
+	for table, resultMap := range cacheManager.dbOperationResult {
+		cachesMapByStatus[table] = make(map[CacheStatus][]CacheContent)
+		for _, result := range resultMap {
+			cacheStatus := result.GetCacheStatus()
+			if cacheStatus == Select || cacheStatus == None {
+				continue
+			}
+
+			if cachesMapByStatus[table][cacheStatus] == nil {
+				cachesMapByStatus[table][cacheStatus] = make([]CacheContent, 0)
+			}
+			cachesMapByStatus[table][cacheStatus] = append(cachesMapByStatus[table][cacheStatus], result.CreateCopy())
+
+			result.SetCacheStatus(None)
+		}
+	}
+
+	for table, cachesMap := range cachesMapByStatus {
+		for cacheStatus, caches := range cachesMap {
+			switch cacheStatus {
+			case Insert:
+				err = cdb.modelBulkExecutorMap[table].BulkInsert(ctx, cdb.tx, caches)
+				if err != nil {
+					return nil, err
+				}
+			case Update:
+				err = cdb.modelBulkExecutorMap[table].BulkUpdate(ctx, cdb.tx, caches)
+				if err != nil {
+					return nil, err
+				}
+			case Delete:
+				err = cdb.modelBulkExecutorMap[table].BulkDelete(ctx, cdb.tx, caches)
+				if err != nil {
+					return nil, err
+				}
+
+			}
+		}
+	}
+
+	return cachesMapByStatus, nil
+}
