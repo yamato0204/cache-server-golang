61ca59402fbf6e2382977c05f7275eb8f80ed762
yamato0204 <tech.yama.0204@gmail.com>
2024-11-13 19:26:35 +0900

    Bulk系　かた実装

--- server/internal/infra/mysql/repository/user_coin.go
+++ server/internal/infra/mysql/repository/user_coin.go
@@ -1,7 +1,11 @@
 package repository
 
 import (
+	"context"
+
+	"github.com/jmoiron/sqlx"
 	"github.com/yamato0204/cache-server-golang/internal/infra/mysql"
+	"github.com/yamato0204/cache-server-golang/internal/infra/mysql/cachedb"
 )
 
 type CoinEntityRepository struct {
@@ -10,4 +14,19 @@ type CoinEntityRepository struct {
 
 func NewCoinEntityRepository(db *mysql.ApplicationDB) *CoinEntityRepository {
 	return &CoinEntityRepository{db: db}
+}
+
+func (r *CoinEntityRepository) BulkInsert(ctx context.Context, tx *sqlx.Tx, content []cachedb.CacheContent) error {
+
+	return nil
+}
+
+func (r *CoinEntityRepository) BulkUpdate(ctx context.Context, tx *sqlx.Tx, content []cachedb.CacheContent) error {
+
+	return nil
+}
+
+func (r *CoinEntityRepository) BulkDelete(ctx context.Context, tx *sqlx.Tx, content []cachedb.CacheContent) error {
+
+	return nil
 }
